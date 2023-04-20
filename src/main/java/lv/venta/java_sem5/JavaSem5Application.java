a870818963759e07470368f0ad1e8be9d06f6beb
s21dororebe <s21dororebe@venta.lv>
2023-04-20 15:22:28 +0300

    New project

---
+++ src/main/java/lv/venta/java_sem5/JavaSem5Application.java
@@ -0,0 +1,13 @@
+package lv.venta.java_sem5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSem5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSem5Application.class, args);
+    }
+
+}
---
+++ src/test/java/lv/venta/java_sem5/JavaSem5ApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.venta.java_sem5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSem5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
